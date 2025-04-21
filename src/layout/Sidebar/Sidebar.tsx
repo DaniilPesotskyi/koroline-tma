a17658d9f15a9200815d3e4bd675414999af0ccd
Daniil Pesotskyi <pesotckiydanya@gmail.com>
2025-04-21 14:38:33 +0300

    feat: Sidebar component

---
+++ src/layout/Sidebar/Sidebar.tsx
@@ -0,0 +1,114 @@
+import React, {useEffect, useState} from "react";
+import {motion} from "framer-motion";
+
+import {CartIcon, FaqIcon, ShoppingBagIcon} from "@/icons";
+
+import {Drawer, IconButton, IconWrapper, List, Path} from "@/components";
+import {
+    contactsListStyles,
+    Heading,
+    NavigationLink,
+    navListStyles, StyledLink, StyledLoveText, SubTitle,
+    Title,
+    toggleButtonStyles,
+    wrapperStyles
+} from "@/layout/Sidebar/styles.ts";
+
+const Sidebar: React.FC = () => {
+    const [open, setOpen] = useState(false)
+
+    useEffect(() => {
+        if (open) {
+            document.body.classList.add('no-scroll')
+        } else {
+            document.body.classList.remove('no-scroll')
+        }
+    }, [open]);
+
+    const toggle = () => {
+        setOpen(!open);
+    }
+
+    return (
+        <>
+            <IconButton onClick={toggle} buttonCss={toggleButtonStyles}>
+                <motion.svg
+                    width="23"
+                    height="23"
+                    viewBox="0 0 23 23"
+                    animate={open ? "open" : "closed"}
+                >
+                    <Path
+                        variants={{
+                            closed: {d: "M 2 2.5 L 20 2.5"},
+                            open: {d: "M 3 16.5 L 17 2.5"},
+                        }}
+                    />
+                    <Path
+                        d="M 2 9.423 L 20 9.423"
+                        variants={{
+                            closed: {opacity: 1},
+                            open: {opacity: 0},
+                        }}
+                        transition={{duration: 0.1}}
+                    />
+                    <Path
+                        variants={{
+                            closed: {d: "M 2 16.346 L 20 16.346"},
+                            open: {d: "M 3 2.5 L 17 16.346"},
+                        }}
+                    />
+                </motion.svg>
+            </IconButton>
+
+            <Drawer
+                open={open}
+                onClose={toggle}
+                position={"right"}
+                customStyles={wrapperStyles}
+            >
+                <Heading>KOROLINE</Heading>
+
+                <Title>Меню</Title>
+                <List customStyles={navListStyles}>
+                    <NavigationLink to={'/'}>
+                        <IconWrapper>
+                            <ShoppingBagIcon/>
+                        </IconWrapper>
+                        Каталог
+                    </NavigationLink>
+                    <NavigationLink to={'/help'}>
+                        <IconWrapper>
+                            <FaqIcon/>
+                        </IconWrapper>
+                        Питання
+                    </NavigationLink>
+                    <NavigationLink to={'/cart'}>
+                        <IconWrapper>
+                            <CartIcon/>
+                        </IconWrapper>
+                        Кошик
+                    </NavigationLink>
+                </List>
+                <Title>Контакти</Title>
+                <SubTitle>Відділ продажу</SubTitle>
+                <List customStyles={contactsListStyles}>
+                    <StyledLink>+38 (096) 000 00 00</StyledLink>
+                    <StyledLink>example@gmail.com</StyledLink>
+                </List>
+                <SubTitle>Співпраця</SubTitle>
+                <List customStyles={contactsListStyles}>
+                    <StyledLink>example@gmail.com</StyledLink>
+                </List>
+                <SubTitle>Технічний відділ</SubTitle>
+                <List customStyles={contactsListStyles}>
+                    <StyledLink>example@gmail.com</StyledLink>
+                </List>
+
+                <StyledLoveText>Made with ♥️ | 1.0</StyledLoveText>
+            </Drawer>
+        </>
+    )
+}
+
+export default Sidebar
---
+++ src/layout/Sidebar/styles.ts
@@ -0,0 +1,89 @@
+import styled, {css} from "styled-components";
+
+import {NavLink} from "react-router-dom";
+
+export const wrapperStyles = css`
+    width: 70%;
+    padding: 20px 10px;
+`
+
+export const toggleButtonStyles = css`
+    z-index: calc(var(--drawer-z-index) + 2);
+`
+
+export const Heading = styled.h2`
+    font-size: 24px;
+    font-weight: 700;
+    
+    margin-bottom: 20px;
+`
+
+export const Title = styled.h3`
+    padding-left: 10px;
+    margin-bottom: 5px;
+    
+    font-size: 16px;
+    font-weight: 600;
+`
+
+export const SubTitle = styled.h4`
+    margin: 0 0 10px 15px;
+    
+    font-size: 13px;
+    font-weight: 500;
+`
+
+// Nav
+
+export const navListStyles = css`
+    margin-bottom: 10px;
+`
+
+export const NavigationLink = styled(NavLink)`
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    
+    padding: 8px 15px;
+
+    border-radius: var(--border-radius);
+
+    font-size: 18px;
+
+    &.active {
+        background-color: var(--accent-color);
+
+        color: #ffffff;
+
+        & svg {
+            fill: #ffffff;
+        }
+    }
+`
+
+// Contacts
+
+export const contactsListStyles = css`
+    gap: 5px;
+    
+    margin-bottom: 15px;
+
+`
+
+export const StyledLink = styled.a`
+    display: block;
+    margin-left: 30px;
+    
+    font-size: 15px;
+`
+
+//
+
+export const StyledLoveText = styled.p`
+    margin-top: 50px;
+
+    font-size: 13px;
+    text-align: center;
+
+    color: #808080;
+`
